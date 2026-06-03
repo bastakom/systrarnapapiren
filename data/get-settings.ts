63e06754e8c38d67e9e1247aa181d25cbbcd3c3e
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-06-03 10:40:11 +0200

    Update get-settings.ts
    switched cache no store mehtod, react version dependant

--- data/get-settings.ts
+++ data/get-settings.ts
@@ -1,17 +1,18 @@
 "use server";
 
 import { getStoryblokApi } from "@storyblok/react";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function getSettings() {
+  noStore();
+
   let sbParams = {
     version: "draft" as const,
   };
 
   const client = getStoryblokApi();
 
-  const data = await client.get(`cdn/stories/settings`, sbParams, {
-    cache: "no-store",
-  });
+  const data = await client.get(`cdn/stories/settings`, sbParams);
 
   if (!data) {
     throw new Error("Not Found");
